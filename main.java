5ca6a4aeb6191f65843e62bca58b52de555e3472
Sean Yap <67400060+S3annnyyy@users.noreply.github.com>
2026-01-29 14:53:16 +0800

    Add WeatherDataManager singleton class

---
+++ main.java
@@ -0,0 +1,33 @@
+public class WeatherDataManager {
+    private static WeatherDataManager instance;
+
+    private double temperature;
+    private double humidity;
+
+    private WeatherDataManager() {}
+
+    public static WeatherDataManager getInstance() {
+        if (instance == null) {
+            instance = new WeatherDataManager();
+        }
+        return instance;
+    }
+
+    public void setWeatherData(double temp, double humid) {
+        this.temperature = temp;
+        this.humidity = humid;
+        notifyDisplays();
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    private void notifyDisplays() {
+        System.out.println("Weather updated: " + temperature + "°C, " + humidity + "%");
+    }
+}
